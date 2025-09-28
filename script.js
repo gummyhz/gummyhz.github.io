7f6d70b4e099ddc033e9328b484e519833b10d0a
gummyhz <gummyrebecca@gmail.com>
2025-09-28 10:20:48 -0400

    update resume

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ $(".themeButton").on("click", function() {
     if( $( "body" ).hasClass( "dark" )) {
       $("h1").removeClass("dark");
       $("h2").removeClass("dark");
+      $("h3").removeClass("dark");
       $("body").removeClass("dark");
       $(".navbar").removeClass("dark");
       $(".page").removeClass("dark");
       $(".item").removeClass("dark");
+      $(".card").removeClass("dark");
+      $(".aboutme").removeClass("dark");
       $(".topMessage").removeClass("dark");
       
       $(".themeButton").text("🌕");
@@ -15,11 +18,13 @@ $(".themeButton").on("click", function() {
       $("body").addClass("dark");
       $("h1").addClass("dark");
       $("h2").addClass("dark");
+      $("h3").addClass("dark");
       $(".navbar").addClass("dark");
       $(".page").addClass("dark");
       $(".item").addClass("dark");
+      $(".card").addClass("dark");
+      $(".aboutme").addClass("dark");
       $(".topMessage").addClass("dark");
-      
       $(".themeButton").text("🌑");
     }
   });
